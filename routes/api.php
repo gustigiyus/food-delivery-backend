d9fdec55c2d73100ebb3d97d9c269aad89370f03
gustigiyus <gustiggt123@gmail.com>
2024-06-26 10:32:44 +0700

    Add routes Authetication and some change

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//user register
+Route::post('/user/register', [App\Http\Controllers\Api\AuthController::class, 'userRegister']);
+
+//restaurant register
+Route::post('/restaurant/register', [App\Http\Controllers\Api\AuthController::class, 'restaurantRegister']);
+
+//driver register
+Route::post('/driver/register', [App\Http\Controllers\Api\AuthController::class, 'driverRegister']);
+
+//login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+//logout
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
